be34097f78e2a30bff74cab983f2119da428d88a
mohongen <496585582@qq.com>
2023-10-19 10:28:15 +0800

    feat: 更换加载模块方法

--- src/entry-server.js
+++ src/entry-server.js
@@ -2,30 +2,37 @@
 import { createApp } from './app'
 import path from 'path'
 import fs from 'fs'
-import { whiteElm, __dirname } from './loadElm'
+import { writeElm, __dirname } from './loadElm'
 
-const getElmSync = () => {
-  return new Promise(resolve => {
-    setImmediate(async () => {
-      try {
-        const elm = require('./element')
-        resolve(elm);
-      } catch(e) {
-        console.info(e)
-      }
-    })
+// const getElmSync = () => {
+//   return new Promise(resolve => {
+//     setImmediate(async () => {
+//       try {
+//         const elm = require('./element')
+//         resolve(elm);
+//       } catch(e) {
+//         console.info(e)
+//       }
+//     })
+//   })
+// }
+
+// const getElm = async (version) => {
+//   const stats = await fs.existsSync(path.resolve(__dirname, './element.js'))
+//   if (stats) {
+//     return await getElmSync();
+//   } else {
+//     await writeElm(version)
+//     return await getElm(version)
+//   }
+// }
+const getElm = (version) => {
+  return writeElm(version).then(() => {
+    const elm = require(`./element@${version}`);
+    return elm
   })
 }
 
-const getElm = async (version) => {
-  const stats = await fs.existsSync(path.resolve(__dirname, './element.js'))
-  if (stats) {
-    return await getElmSync();
-  } else {
-    await whiteElm(version)
-    return await getElm(version)
-  }
-}
 export default async (context) => {
   const elm = await getElm(context.version || '2.15.14')
   const { app } = createApp(elm)
--- src/loadElm.js
+++ src/loadElm.js
@@ -1,11 +1,15 @@
-import axios from 'axios';
-import fs from 'fs';
-import path, { dirname } from 'path';
-import { fileURLToPath } from "url";
-export const __dirname = dirname(fileURLToPath(import.meta.url));
+import axios from 'axios'
+import fs from 'fs'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+export const __dirname = dirname(fileURLToPath(import.meta.url))
 
-export const whiteElm = async (version) => {
-  const element = await (await axios.get(`https://unpkg.com/element-ui@${version}/lib/index.js`)).data;
-  console.info(__dirname)
-  fs.writeFileSync(path.resolve(__dirname, './element.js'), element, 'utf-8');
+export const writeElm = (version) => {
+  return new Promise((resolve) => {
+    axios.get(`https://unpkg.com/element-ui@${version}/lib/index.js`).then((res) => {
+      fs.writeFile(path.resolve(__dirname, `./element@${version}.js`), res.data, () => {
+        resolve('ok')
+      })
+    })
+  })
 }
